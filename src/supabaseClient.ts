667410251524171281cacdbe828949f77ce8c385
christopherjones-eng <christopher.jones@lshubwales.com>
2025-10-07 08:25:09 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-// Your Supabase project details
-const supabaseUrl = "https://pwusqgogteecnaiiotrm.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB3dXNxZ29ndGVlY25haWlvdHJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxODgwNzEsImV4cCI6MjA3Mzc2NDA3MX0.RQb6VDYbRAUtXQtdwgReixdZk4cEfMI96CXifuu5-O4";
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
